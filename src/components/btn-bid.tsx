204c59178b552d960d57e3569be8fa1258c77832
tin nguyen <tinndev21@gmail.com>
2024-05-03 18:24:18 +0700

    bid function

--- src/components/btn-bid.tsx
+++ src/components/btn-bid.tsx
@@ -1,16 +1,27 @@
-import { useTonConnectUI } from "@tonconnect/ui-react"
+import { useTonAddress, useTonConnectUI } from "@tonconnect/ui-react"
+import TonWeb from "tonweb";
 
 const BtnBid = () => {
 
   const [tonConnectUI] = useTonConnectUI();
+  const address = useTonAddress();
 
-  const handleBid = () => {
+  const handleBid = async () => {
 
+    let a = new TonWeb.boc.Cell();
+    a.bits.writeUint(5,32);
+    a.bits.writeUint(0,64);
+    a.bits.writeAddress(new TonWeb.utils.Address(address));
+    a.bits.writeCoins(0.05* 1000000000);
+
+    const result = await a.toBoc();
+    const payload = TonWeb.utils.bytesToBase64(result); 
+    
     const message = [
       {
         address: 'kQApFtSIC4zxh4IydCRHma7URhg3HFE05tOnUwPcLtYZ-j9U',
-        amount: String(0.1 * 10000000),
-        // payload: 
+        amount: String(0.05 * 1000000000),
+        payload
       }
     ]
 
@@ -18,8 +29,6 @@ const BtnBid = () => {
       messages: message,
       validUntil: Date.now() + 5 * 60 * 1000, // 5 minutes for user to approve
     })
-
-
   }
 
 
